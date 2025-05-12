1f0b35baa135df545855a9d72ae11e18c268579c
mikaelcharbonneau <153523692+mikaelcharbonneau@users.noreply.github.com>
2025-05-12 07:39:19 -0400

    good

--- api/db.ts
+++ api/db.ts
@@ -1,15 +1,8 @@
 import sql from 'mssql';
 
-const config = {
-    connectionString: process.env.SQL_CONNECTION_STRING,
-    options: {
-        encrypt: true // For Azure SQL
-    }
-};
-
 export async function getConnection() {
     if (!sql.pool) {
-        sql.pool = await sql.connect(config);
+        sql.pool = await sql.connect(process.env.SQL_CONNECTION_STRING as string);
     }
     return sql.pool;
 }
