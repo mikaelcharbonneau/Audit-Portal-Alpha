8c4c236d43bbbd3ecc3661e192ffe9932caf4855
mikaelcharbonneau <153523692+mikaelcharbonneau@users.noreply.github.com>
2025-05-14 02:18:04 -0400

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
   return (
     <GrommetHeader 
       background={darkMode ? 'background-back' : 'background-front'} 
-      pad={{ horizontal: 'medium', vertical: 'small',right: 'small' }} 
+      pad={{ horizontal: 'medium', vertical: 'small',right: 'large' }} 
       elevation="small"
     >
       <Box 
